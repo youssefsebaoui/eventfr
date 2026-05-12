9422e1b5d84bb65e35a7a0260e2bd5187b53dd5a
sebaouiy <sebaouiy@gmail.com>
2026-05-13 00:45:31 +0100

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/eventmanager/dto/CategorieDTO.java
+++ src/main/java/com/eventmanager/dto/CategorieDTO.java
@@ -1,5 +1,8 @@
 package com.eventmanager.dto;
 
+import lombok.Data;
+
+@Data
 public class CategorieDTO {
 
     private Long id;
@@ -11,51 +14,5 @@ public class CategorieDTO {
     private Long typeEvenementId;
     private String typeEvenementLibelle;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Long getProprietaireId() {
-        return proprietaireId;
-    }
-
-    public void setProprietaireId(Long proprietaireId) {
-        this.proprietaireId = proprietaireId;
-    }
-
-    public Long getTypeEvenementId() {
-        return typeEvenementId;
-    }
-
-    public void setTypeEvenementId(Long typeEvenementId) {
-        this.typeEvenementId = typeEvenementId;
-    }
-
-    public String getTypeEvenementLibelle() {
-        return typeEvenementLibelle;
-    }
-
-    public void setTypeEvenementLibelle(String typeEvenementLibelle) {
-        this.typeEvenementLibelle = typeEvenementLibelle;
-    }
 }
--- src/main/java/com/eventmanager/entity/Categorie.java
+++ src/main/java/com/eventmanager/entity/Categorie.java
@@ -3,11 +3,15 @@ package com.eventmanager.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 public class Categorie{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,51 +28,5 @@ public class Categorie{
     private TypeEvenement typeEvenement;
 
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Long getProprietaireId() {
-        return proprietaireId;
-    }
-
-    public void setProprietaireId(Long proprietaireId) {
-        this.proprietaireId = proprietaireId;
-    }
-
-    public List<Prestation> getServices() {
-        return services;
-    }
-
-    public void setServices(List<Prestation> services) {
-        this.services = services;
-    }
-
-    public TypeEvenement getTypeEvenement() {
-        return typeEvenement;
-    }
-
-    public void setTypeEvenement(TypeEvenement typeEvenement) {
-        this.typeEvenement = typeEvenement;
-    }
 }
